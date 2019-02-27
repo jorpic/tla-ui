2b9b3be0ed30b145d0fd7992b5b3c1cd94aaa16f
Max Taldykin <jorpic@gmail.com>
2019-02-27 16:28:23 +0300

    Add codemirror

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 import './style';
+import 'codemirror/lib/codemirror.css';
+import * as codemirror from 'codemirror';
+
+
 import { Component } from 'preact';
 
+class Codemirror extends Component {
+  componentDidMount() {
+    this.codeMirror = codemirror.fromTextArea(this.textareaNode, this.props.options);
+  }
+
+
+  componentWillUnmount() {
+    if (this.codeMirror) this.codeMirror.toTextarea();
+  }
+
+
+  render() {
+    return (
+      <textarea
+        ref={ref => this.textareaNode = ref}
+        name={this.props.name}
+        defaultValue={this.props.value}
+        autoComplete="off"
+        autoFocus={this.props.autoFocus}
+      />)
+  }
+}
+
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+  render() {
+    const text = "---- test ----\n====";
+    const options = {
+      lineNumbers: true,
+    };
+    return (
+      <div>
+        <h1>TLA+ UI</h1>
+        <Codemirror name="TLA+"
+          defaultValue={text}
+          autoFocus={true}
+          options={options} />
+      </div>
+    );
+  }
 }
